77759a6a7c790ba7ec63a1ed5442bb3b40a80b8b
Amit <ak79438@gmail.com>
2024-07-13 22:15:20 +0530

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import ReactLogo from "../assets/quiz-logo.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={ReactLogo} />
+      <h1>REACTQUIZ</h1>
+    </header>
+  );
+}
---
+++ src/Components/QuestionTime.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export default function QuestionTimer({ onTimeout, timeout, mode }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    console.log("Setting Timeout");
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onTimeout, timeout]);
+
+  useEffect(() => {
+    console.log("Setting Interval");
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    ></progress>
+  );
+}
